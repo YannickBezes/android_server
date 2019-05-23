3940f6b283582b8ca8c92a799e09da74f4dd597e
yannickbezes <yannick.bezes@gmail.com>
2019-05-23 16:03:59 +0200

    update file fill_database

--- fill_database.py
+++ fill_database.py
@@ -10,19 +10,19 @@ FAIL = '\033[91m'
 ENDC = '\033[0m'
 
 # Add users
-res = requests.post(url="http://{}:5000/user".format(base_url), json={"city": "Montpellier", "email": "florian.chevalier@etu.umontpellier.fr", "firstname": "Florian", "gender": "male", "height": 180, "lastname": "Chevalier", "lat": 43.633153, "lng": 3.863715, "username": "Flo", "weight": 60, "password": "flo"})
+res = requests.post(url="http://{}:5000/user".format(base_url), json={"city": "Montpellier", "email": "florian.chevalier@etu.umontpellier.fr", "firstname": "Florian", "gender": "male", "height": 180, "lastname": "Chevalier", "lat": 43.633153, "lng": 3.863715, "username": "Flo", "weight": 60, "password": "flo", "interest": "sport"})
 msg = SUCCESS if res.json()['success'] else FAIL
 msg += "Add user " + BOLD + "Flo" + ENDC
 print(msg)
 token_flo = res.json()['token']
 
-res = requests.post(url="http://{}:5000/user".format(base_url), json={"city": "Montpellier", "email": "yannick.bezes@etu.umontpellier.fr", "firstname": "Yannick", "gender": "male", "height": 173, "lastname": "Bezes", "lat": 43.633153, "lng": 3.863715, "username": "Yannick", "weight": 53, "password": "yannick"})
+res = requests.post(url="http://{}:5000/user".format(base_url), json={"city": "Montpellier", "email": "yannick.bezes@etu.umontpellier.fr", "firstname": "Yannick", "gender": "male", "height": 173, "lastname": "Bezes", "lat": 43.633153, "lng": 3.863715, "username": "Yannick", "weight": 53, "password": "yannick", "interest": "programmation,sport"})
 msg = SUCCESS if res.json()['success'] else FAIL
 msg += "Add user " + BOLD + "Yannick" + ENDC
 print(msg)
 token_yannick = res.json()['token']
 
-res = requests.post(url="http://{}:5000/user".format(base_url), json={"city": "Agde", "email": "jean.paul@gmail.com", "firstname": "Jean", "gender": "male", "height": 181, "lastname": "Paul", "lat": 43.308744, "lng": 3.476735, "username": "Jean", "weight": 80, "password": "jean"})
+res = requests.post(url="http://{}:5000/user".format(base_url), json={"city": "Agde", "email": "jean.paul@gmail.com", "firstname": "Jean", "gender": "male", "height": 181, "lastname": "Paul", "lat": 43.308744, "lng": 3.476735, "username": "Jean", "weight": 80, "password": "jean", "interest": "mode,marche"})
 msg = SUCCESS if res.json()['success'] else FAIL
 msg += "Add user " + BOLD + "Jean" + ENDC
 print(msg)
@@ -96,17 +96,17 @@ print(msg)
 
 
 # Add shops
-res = requests.post(url="http://{}:5000/shop".format(base_url), json={"name": "Decathlon Odysseum", "lat": 43.605403, "lng": 3.923821, "city": "Montpellier", "category": "Sport", "address": "1072 Rue Georges Melies, 34000 Montpellier"}, headers={"x-access-token": token_flo})
+res = requests.post(url="http://{}:5000/shop".format(base_url), json={"name": "Decathlon Odysseum", "lat": 43.605403, "lng": 3.923821, "city": "Montpellier", "category": "Sport", "address": "1072 Rue Georges Melies, 34000 Montpellier", "keywords": "marche,sport"}, headers={"x-access-token": token_flo})
 msg = SUCCESS if res.json()['success'] else FAIL
 msg += "Add shop " + BOLD + "Decathlon Odysseum" + ENDC
 print(msg)
 
-res = requests.post(url="http://{}:5000/shop".format(base_url), json={"name": "Chauss34", "lat": 43.650982, "lng": 3.846524, "city": "Saint-Clement-de-Riviere", "category": "Mode", "address": "6 Rue des Genets, 34980 Saint-Clement-de-Riviere"}, headers={"x-access-token": token_flo})
+res = requests.post(url="http://{}:5000/shop".format(base_url), json={"name": "Chauss34", "lat": 43.650982, "lng": 3.846524, "city": "Saint-Clement-de-Riviere", "category": "Mode", "address": "6 Rue des Genets, 34980 Saint-Clement-de-Riviere", "keywords": "mode,chausure"}, headers={"x-access-token": token_flo})
 msg = SUCCESS if res.json()['success'] else FAIL
 msg += "Add shop " + BOLD + "Chauss34" + ENDC
 print(msg)
 
-res = requests.post(url="http://{}:5000/shop".format(base_url), json={"name": "Decathlon Saint Jean De Vedas", "lat": 43.571359, "lng": 3.845273, "city": "Saint-Jean-de-Vedas", "category": "Sport", "address": "Zac Deves De La Condamine, 34430 Saint-Jean-de-Vedas"}, headers={"x-access-token": token_flo})
+res = requests.post(url="http://{}:5000/shop".format(base_url), json={"name": "Decathlon Saint Jean De Vedas", "lat": 43.571359, "lng": 3.845273, "city": "Saint-Jean-de-Vedas", "category": "Sport", "address": "Zac Deves De La Condamine, 34430 Saint-Jean-de-Vedas", "keywords": "sport,tennis"}, headers={"x-access-token": token_flo})
 msg = SUCCESS if res.json()['success'] else FAIL
 msg += "Add shop " + BOLD + "Decathlon Saint Jean De Vedas" + ENDC
 print(msg)
